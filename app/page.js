4215b95290f92fbd08e10a2247db7998f34245ce
Wasin Kampeera <wasin.kamp@gmail.com>
2023-10-15 15:26:09 +0700

    try updated page.js with newly fetch

--- app/page.js
+++ app/page.js
@@ -1,18 +1,34 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { Container, Row, Col } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row } from 'react-bootstrap';
 
-import Welcome from './Welcome'
-import FirstPageComment from './components/FirstPageComment'
+import Welcome from './Welcome';
+import FirstPageComment from './components/FirstPageComment';
 
-export default function Home() {
+export default function Home({ comments }) {
     return (
-      <Container>
-        <Row className='p-2 d-flex justify-content-center'>
-          <Welcome />
-        </Row>
-        <Row className='mt-2'>
-            <FirstPageComment />
-        </Row>
-      </Container>
-    )
+        <Container>
+            <Row className='p-2 d-flex justify-content-center'>
+                <Welcome />
+            </Row>
+            <Row className='mt-2'>
+                <FirstPageComment comments={comments} />
+            </Row>
+        </Container>
+    );
+}
+
+const baseUrl_api = "https://chana-ckd-api.vercel.app";
+
+export async function getServerSideProps(context) {
+    const res = await fetch(`${baseUrl_api}/api/comments/firstpage`, {
+        cache: 'no-store',
+    });
+
+    const data = await res.json();
+
+    return {
+        props: {
+            comments: data.comments || []
+        }
+    };
 }
